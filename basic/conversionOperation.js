0c76a6d2382489e12a78955413a3420d6b8b9dfb
Sarvesh7090 <thakur.sarvesh.7333@gmail.com>
2024-12-30 15:14:27 +0000

    conversion of data type

---
+++ basic/conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "Sarvesh"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = 'sarvesh'
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(boolenIsLoggedIn)
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
