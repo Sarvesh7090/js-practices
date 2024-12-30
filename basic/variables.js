64f1df446c88597c18e246a020be288466f52ea0
Sarvesh7090 <thakur.sarvesh.7333@gmail.com>
2024-12-30 06:50:45 +0000

    variable and datatypes

---
+++ basic/variables.js
@@ -0,0 +1,10 @@
+ const accountId = 1445566
+ let accountEmail = "sarvesh@gmail.com"
+ var accountPassword = "12345"
+ accountCity = "Mumbai"
+
+/*
+Prefer not to use var
+because of issue in block scope and fuctional scope
+*/
+ console.table([accountEmail,accountId,accountPassword,accountCity])
